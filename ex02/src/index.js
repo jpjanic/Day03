67b4179a8f26a601a7ee7ef1bcfdcf7e9c04c02d
jpjanic <jasmin.pjanic@yahoo.com>
2021-03-10 18:39:32 +0100

    Added index file

---
+++ ex02/src/index.js
@@ -0,0 +1,16 @@
+function main() {
+    // Only change code below this line
+
+// Paragon single line Javascript comment
+
+/* Paragon multi-line comment
+   Paragon multi-line comment
+   Paragon multi-line comment
+ */
+
+    // Only change code above this line
+
+}
+
+console.log(main());
+module.exports = main;
